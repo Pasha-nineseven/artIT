7653ef454195f75d144549da9651b44e2afc5f62
want2stayhere <want2stayhere@gmail.com>
2020-04-25 15:53:22 +0300

    Новости+статьи

--- app/js/interface.js
+++ app/js/interface.js
@@ -134,6 +134,53 @@ $(document).ready(function() {
         });
     }
 
+    if($('.article-slider').length>0) {
+        var $statusNews = $('.pagingInfo-article .s-current');
+        var $statusTotalNews = $('.pagingInfo-article .s-total');
+        var $sliderNews = $('.article-slider');
+        $sliderNews.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+            var i = (currentSlide ? currentSlide : 0) + 1;
+            $statusNews.text('0' + i);
+            $statusTotalNews.text('/' + '0' + slick.slideCount);
+            // console.log( $statusNews.text);
+            // console.log( i);
+        });
+        $sliderNews.not('.slick-initialized').slick({
+            infinite: true,
+            dots: false,
+            arrows:true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            adaptiveHeight: false,
+            prevArrow: $(".js-article-prev"),
+            nextArrow: $(".js-article-next"),
+        });
+    }
+
+    if($('.page-news-slider').length>0) {
+        var $statusNews = $('.pagingInfo-newsPage .s-current');
+        var $statusTotalNews = $('.pagingInfo-newsPage .s-total');
+        var $sliderNews = $('.page-news-slider');
+        $sliderNews.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+            var i = (currentSlide ? currentSlide : 0) + 1;
+            $statusNews.text('0' + i);
+            $statusTotalNews.text('/' + '0' + slick.slideCount);
+            // console.log( $statusNews.text);
+            // console.log( i);
+        });
+        $sliderNews.not('.slick-initialized').slick({
+            infinite: true,
+            dots: false,
+            arrows:true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            adaptiveHeight: false,
+            prevArrow: $(".js-newsPage-prev"),
+            nextArrow: $(".js-newsPage-next"),
+        });
+    }
+
+
     if($('.trust-slider').length>0) {
 		var $status = $('.trust-pagingInfo .s-current');
 		var $statusTotal = $('.trust-pagingInfo .s-total');
@@ -471,16 +518,17 @@ function getScrollbarWidth() {
 }
 
 // links pages
-// $('body').append(
-// 	'<div style="position: fixed; z-index: 1005; bottom: 0; right: 0; background: #fff; border: solid 1px #828286; width: 200px;"> \
-// 		<a href="javascript:void(0);" style="float: right;background:#ccc; color:#000; padding: 5px 10px; text-decoration: none; font-size: 16px" onclick="$(this).parent().hide()">Close X</a> \
-// 	<style> \
-// 		#pages { padding: 10px 20px 0 50px; font-size: 18px; } \
-// 		#pages a { text-decoration: none; } \
-// 		#pages li { margin: 5px 0; } \
-// 	</style> \
-// 	<ol id="pages"> \
-// 		<li><a href="about.html">About</a></li> \
-// 		<li><a href="index.html">Index</a></li> \
-// 	</ol> \
-// </div>');
+$('body').append(
+	'<div style="position: fixed; z-index: 1005; bottom: 0; right: 0; background: #fff; border: solid 1px #828286; width: 200px;"> \
+		<a href="javascript:void(0);" style="float: right;background:#ccc; color:#000; padding: 5px 10px; text-decoration: none; font-size: 16px" onclick="$(this).parent().hide()">Close X</a> \
+	<style> \
+		#pages { padding: 10px 20px 0 50px; font-size: 18px; } \
+		#pages a { text-decoration: none; } \
+		#pages li { margin: 5px 0; } \
+	</style> \
+	<ol id="pages"> \
+		<li><a href="index.html">Главная</a></li> \
+		<li><a href="article.html">Статьи</a></li> \
+        <li><a href="news.html">Новости</a></li> \
+	</ol> \
+</div>');
